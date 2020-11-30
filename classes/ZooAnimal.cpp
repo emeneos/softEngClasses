32906815c611fb62d0163c2a39554b4732431ad3
ebaneo <ebaneo_89@hotmail.com>
2020-11-30 20:15:25 +0100

    exc1.2 ready

--- classes/ZooAnimal.cpp
+++ classes/ZooAnimal.cpp
@@ -8,3 +8,27 @@ void ZooAnimal::create(char* name, int cageNumber, int weightDate, int weight)
     this->cageNumber = cageNumber;
     this->weightDate = weightDate;
 }
+
+void ZooAnimal::Destroy()
+{
+    delete[] name;
+}
+
+// -------- member function to return the animal's name
+char* ZooAnimal::reptName()
+{
+    return name;
+}
+
+// -------- member function to return the number of days
+// -------- since the animal was last weighed
+
+int ZooAnimal::daysSinceLastWeighed(int today) //exercise 1.2
+{
+    int startday, thisday;
+    thisday = today / 100 * 30 + today - today / 100 * 100;
+    startday = weightDate / 100 * 30 + weightDate - weightDate / 100 * 100;
+    if (thisday < startday)
+        thisday += 360;
+    return (thisday - startday);
+}
